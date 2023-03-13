af931d6a521a9ce94aaa8161b0414ac5658138f2
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-03-13 23:53:33 +0530

    fix:style for content page

--- pages/GetStarted/Installation/installation.js
+++ pages/GetStarted/Installation/installation.js
@@ -41,14 +41,14 @@ mainHeight.addEventListener("scroll", () => {
   if (scrolled > 0 && scrolled < 630) {
     first.style.color = "#ffffff";
     first.style.backgroundColor = "#1a3b5e";
-    first.style.borderLeft = "1px solid #3697ff";
+    first.style.borderLeft = "1.5px solid #3697ff";
     second.style.backgroundColor = "";
     second.style.borderLeft = "";
   } else if (scrolled > 630) {
     first.style.backgroundColor = "";
     first.style.borderLeft = "";
     second.style.backgroundColor = "#1a3b5e";
-    second.style.borderLeft = "1px solid #3697ff";
+    second.style.borderLeft = "1.5px solid #3697ff";
   }
   console.log(mainHeight.scrollTop);
   //   console.log(totalHeight);
