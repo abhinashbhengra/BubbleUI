2e336963cfe96f0644397e5015c8cfc10b649fa2
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-03 00:01:56 +0530

    feat:show menu

--- pages/GetStarted/Installation/installation.js
+++ pages/GetStarted/Installation/installation.js
@@ -1,7 +1,17 @@
+//menu
+const menu = document.querySelector(".menu-icon");
+const sideMenu = document.querySelector(".left-container");
+console.log(menu);
+
 // Scroll Event
 const first = document.querySelector(".right-first");
 const second = document.querySelector(".right-second");
 const mainHeight = document.querySelector(".main-content");
+
+menu.addEventListener("click", () => {
+  sideMenu.style.position = "absolute";
+  sideMenu.style.display = "block";
+});
 
 mainHeight.addEventListener("scroll", () => {
   //   const totalHeight = mainHeight.scrollHeight - window.innerHeight;
